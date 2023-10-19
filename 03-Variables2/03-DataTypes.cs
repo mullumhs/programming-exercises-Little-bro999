21a42627680ff167bb171cd766ac5b2acbda01bd
Little-bro999 <147568865+Little-bro999@users.noreply.github.com>
2023-10-19 11:44:55 +1100

    this is my completed project 3
    this is my completed project 3

--- 03-Variables2/03-DataTypes.cs
+++ 03-Variables2/03-DataTypes.cs
@@ -17,8 +17,9 @@ Main Tasks:
 
 Questions:
 1. What is the best data type for a whole number?
+i think that int is the best for  whole number
 2. What are some data types you could use for decimal numbers?
-
+double
 Extension Tasks:
 1. Use Google to find out what other data types are in C#, then have a go at using them.
 2. Find out how to join variables to strings using "String Interpolation".
@@ -44,7 +45,13 @@ namespace ProgrammingExercisesIST
         static void Main(string[] args)
         {
             // Your code goes below here
-
+            int n = 2008436257;
+            double m = 8002658685.3;
+            bool b = true;
+            Console.WriteLine(n+m);
+            Console.ReadLine();
+            Console.WriteLine(b);
+            Console.ReadLine();
         }
     }
 }
